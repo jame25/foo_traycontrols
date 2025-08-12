601f364b9eb6dd782c1fb041183f9c67c2ad5798
jame25 <jame25@users.noreply.github.com>
2025-08-12 13:49:51 +0100

    Update resource.h

--- resource.h
+++ resource.h
@@ -2,10 +2,7 @@
 
 #define IDI_TRAY_ICON           101
 #define IDI_RADIO_ICON          102
-#define IDI_PLAY_ICON           103
-#define IDI_PAUSE_ICON          104
-#define IDI_PREVIOUS_ICON       105
-#define IDI_NEXT_ICON           106
+// Control panel icon constants removed - now using Segoe Fluent Icons font
 #define IDR_TRAY_PNG            107
 
 #define IDD_PREFERENCES_TRAY    200
@@ -13,6 +10,11 @@
 #define IDC_ALWAYS_MINIMIZE_ON_CLOSE 202
 #define IDC_MOUSE_WHEEL_VOLUME       203
 #define IDC_SHOW_POPUP_NOTIFICATION  204
+#define IDC_ARTIST_FONT_DISPLAY      205
+#define IDC_SELECT_ARTIST_FONT       206
+#define IDC_TRACK_FONT_DISPLAY       207
+#define IDC_SELECT_TRACK_FONT        208
+#define IDC_RESET_FONTS              209
 #define IDC_STATIC              -1
 
 #define IDM_TRAY_PLAY           1001
