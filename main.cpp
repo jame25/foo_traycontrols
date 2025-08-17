6ce3c83759bf1365e05260ce45b4c8ad5dfbe4df
jame25 <jame25@users.noreply.github.com>
2025-08-18 00:35:25 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -25,7 +25,7 @@ BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserv
 // Component version declaration using the proper SDK macro
 DECLARE_COMPONENT_VERSION(
     "Tray Controls",
-    "1.1.5",
+    "1.1.6",
     "System tray controls for foobar2000.\n"
     "Features:\n"
     "- Minimize to system tray\n"
