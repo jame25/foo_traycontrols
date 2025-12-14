37c557d56c8a174a044ce2f4ec2c50f9e8f573b7
jame25 <jame25@users.noreply.github.com>
2025-12-14 21:11:39 +0000

    Update preferences.h

--- preferences.h
+++ preferences.h
@@ -20,13 +20,28 @@ void set_track_font(const LOGFONT& font);
 void reset_fonts();
 LOGFONT get_default_font(bool is_artist, int size);
 
-// Control Panel font configuration functions
+// Docked Control Panel font configuration functions
 bool get_cp_use_custom_fonts();
 LOGFONT get_cp_artist_font();
 LOGFONT get_cp_track_font();
 void set_cp_artist_font(const LOGFONT& font);
 void set_cp_track_font(const LOGFONT& font);
 void reset_cp_fonts();
+
+// Undocked mode font configuration functions
+bool get_undocked_use_custom_fonts();
+LOGFONT get_undocked_artist_font();
+LOGFONT get_undocked_track_font();
+
+// Expanded mode font configuration functions  
+bool get_expanded_use_custom_fonts();
+LOGFONT get_expanded_artist_font();
+LOGFONT get_expanded_track_font();
+
+// Compact mode font configuration functions
+bool get_compact_use_custom_fonts();
+LOGFONT get_compact_artist_font();
+LOGFONT get_compact_track_font();
 
 // Preferences page instance - the actual dialog
 class tray_preferences : public preferences_page_instance {
@@ -35,6 +50,7 @@ private:
     preferences_page_callback::ptr m_callback;
     bool m_has_changes;
     fb2k::CCoreDarkModeHooks m_darkMode;
+    int m_current_tab; // 0 = General, 1 = Fonts
     
 public:
     tray_preferences(preferences_page_callback::ptr callback);
@@ -61,6 +77,12 @@ private:
     void select_cp_track_font();
     void reset_cp_fonts_to_default();
     pfc::string8 format_font_name(const LOGFONT& lf);
+    
+    // Tab control methods
+    void init_tab_control();
+    void switch_tab(int tab);
+    void select_font_for_mode(int mode, bool is_artist); // mode: 1=undocked, 2=expanded, 3=compact
+    void reset_all_fonts_to_default();
 };
 
 // Preferences page factory
