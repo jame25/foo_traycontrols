8a1346ba5cd59b97cb4886094960b198ea68c158
jame25 <jame25@users.noreply.github.com>
2026-08-03 04:51:00 +0100

    Update volume_popup.h

--- volume_popup.h
+++ volume_popup.h
@@ -1,8 +1,10 @@
 #pragma once
 
 #include "stdafx.h"
+#include <gdiplus.h>
 
 class volume_popup {
+
 public:
     static volume_popup& get_instance();
 
@@ -12,6 +14,8 @@ public:
     // Show popup centered at x, y (bottom center of the popup will be at x, y)
     // Or anchored to a button position
     void show_at(int x, int y);
+    // Show visual feedback popup when volume is changed via tray icon scroll
+    void show_feedback();
     void hide();
     bool is_visible() const { return m_visible; }
 
@@ -25,12 +29,12 @@ private:
     bool m_initialized;
     bool m_visible;
     bool m_is_dragging;
+    bool m_is_feedback_mode;
     
     // UI state
     float m_current_volume_db; // Current volume in dB
     int m_hover_level; // 0-100 for slider hover effect
 
-    // Visual constants
     // Visual constants
     static const int POPUP_WIDTH = 150;
     static const int POPUP_HEIGHT = 50; // increased for bubble arrow
@@ -41,14 +45,23 @@ private:
     static const int ARROW_WIDTH = 16;
     static const int CORNER_RADIUS = 8;
 
+    // Feedback OSD visual constants
+    static const int FEEDBACK_WIDTH = 220;
+    static const int FEEDBACK_HEIGHT = 44;
+
     void create_window();
     void register_class();
     void paint(HDC hdc);
+    void paint_feedback(HDC hdc);
+    void draw_speaker_icon(Gdiplus::Graphics &g, int x, int y, int size, float volume_percent, const Gdiplus::Color &color);
     void update_volume_from_point(POINT pt);
+
     
     // Timer IDs
     static const UINT UPDATE_TIMER_ID = 5001;
     static const UINT FADE_TIMER_ID = 5002;
+    static const UINT FEEDBACK_TIMER_ID = 5003;
 
     static LRESULT CALLBACK window_proc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
 };
+
